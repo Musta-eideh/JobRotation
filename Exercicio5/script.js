255b2fe7077ed5476268946ef4982ed2bc729d02
Musta-eideh <mustafaeideh99@gmail.com>
2023-04-24 23:52:17 -0300

    Last exercise incomplete

---
+++ Exercicio5/script.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector('.btn');
+let inputText = document.querySelector('.input-text');
+let unlike = document.querySelector('.text-unlike');
+
+btn.addEventListener("click", function(){
+    conts letter = inputText.slice('');
+    console.log(letter);
+})
